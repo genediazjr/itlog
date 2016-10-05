ddff595557bf7d4c357a6dcdcf4daad5f54565cf
Gene Diaz <genediazjr@gmail.com>
2016-10-05 20:41:17 +0800

    add first test

--- test/index.js
+++ test/index.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const log = require('..');
+
+const TestConsole = require('test-console');
+const Code = require('code');
+const Lab = require('lab');
+
+const lab = exports.lab = Lab.script();
+const describe = lab.describe;
+const it = lab.it;
+const expect = Code.expect;
+
+const stdout = TestConsole.stdout;
+
+describe('lib', () => {
+
+    it('logs', (done) => {
+
+        const output = stdout.inspectSync(() => {
+            log('foo');
+        });
+
+        expect(output).to.equal(['foo\n']);
+
+        return done();
+    });
+});
